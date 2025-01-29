ca349cb25fa4cc8a5165dbb63724127697c19f48
Omar Diaz <odiaz7170@yahoo.com>
2025-01-28 18:39:07 -0600

    finished excercise 5

--- app.js
+++ app.js
@@ -101,3 +101,25 @@ const generateEmail = (string1, string2)=>{
 }
 
 console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+/*
+Exercise 5: greetUser()
+
+Define a function called greetUser. It should take a name and a 
+time of day (morning, afternoon, evening) and return a personalized 
+greeting.
+
+Example: greetUser('Sam', 'morning') should return "Good morning, Sam!"
+
+Complete the exercise in the space below:
+*/
+// function is greetUser 
+// were taking in 2 strings
+// string1 and string2
+// combine the strings
+const greetUser = (string1, string2)=>{
+    return "good " + string2 + "," + string1
+}
+
+
+console.log('Exercise 5 Result:', greetUser("Sam", "morning"));
